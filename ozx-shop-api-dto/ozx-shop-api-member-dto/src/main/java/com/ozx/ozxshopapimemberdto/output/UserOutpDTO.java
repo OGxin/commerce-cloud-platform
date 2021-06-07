562eff05b97380a0e9fb55e45030a376660ea7bf
OGxin <779855839@qq.com>
2021-06-07 09:58:09 +0800

    用户信息基于DTO输出输入

--- ozx-shop-api-dto/ozx-shop-api-member-dto/src/main/java/com/ozx/ozxshopapimemberdto/output/UserOutpDTO.java
+++ ozx-shop-api-dto/ozx-shop-api-member-dto/src/main/java/com/ozx/ozxshopapimemberdto/output/UserOutpDTO.java
@@ -1,23 +1,21 @@
-package com.ozx.ozxshopapimemberdto.input;
+package com.ozx.ozxshopapimemberdto.output;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import java.sql.Date;
-
 /**
- * @ClassName: UserInpDTO
+ * @ClassName: UserOutpDTO
  * @Description:TODO
  * @Author ou.zhenxing
- * @Date 2020/3/6 17:11
+ * @Date 2020/4/7 17:31
  * @Version： 1.0
  **/
 @Data
-@ApiModel(value = "用户信息实体类")
-public class UserInpDTO {
+@ApiModel(value = "用户输出信息")
+public class UserOutpDTO {
     @ApiModelProperty(value = "用户ID")
-    private Integer userId;
+    private Long userId;
     @ApiModelProperty(value = "手机号码")
     private String mobile;
     @ApiModelProperty(value = "电子邮箱")
@@ -34,5 +32,4 @@ public class UserInpDTO {
     private String qqOpenid;
     @ApiModelProperty(value = "微信开放ID")
     private String wxOpenid;
-    private String regCode;
 }
